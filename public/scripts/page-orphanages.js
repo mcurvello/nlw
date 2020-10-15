0f051d64089d114be03444634bce255a19f17fc1
Marcio Curvello <marciocurvello@gmail.com>
2020-10-15 20:58:29 -0300

    Terceiro dia

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -14,16 +14,31 @@ const icon = L.icon({
     popAnchor: [170, 2]
 })
 
+function addMarker({id, name, lat, lng} = orphanage){
 // create popup overlay
 const popup = L.popup({
     closeButton: false,
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
 
 //create and add marker
 L
-.marker([-29.7665363,-57.0852528], { icon })
+.marker([lat, lng], { icon })
 .addTo(map)
 .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
